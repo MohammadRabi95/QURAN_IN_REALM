a0622f46fc4cbca82e5790f52a81c37b34c64308
MohammadRabi95 <67598412+MohammadRabi95@users.noreply.github.com>
2021-03-13 03:26:24 +0500

    Update Model.java

--- app/src/main/java/com/cv/quran_in_realm/Model.java
+++ app/src/main/java/com/cv/quran_in_realm/Model.java
@@ -5,13 +5,7 @@ import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.Required;
 
 public class Model extends RealmObject {
-
-
-    public static final String PROPERTY_NAME = "name";
-    public static final String PROPERTY_NUM = "num";
-
     @PrimaryKey
-    //@Required
     public int num;
     public String name;
 
